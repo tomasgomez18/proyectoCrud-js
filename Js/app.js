1bf3ea9abe3b4ab67717c98c56c0f83ba47486ba
tomas <tomasgomezzi36@gmail.com>
2025-11-11 16:53:09 -0300

    fix

---
+++ Js/app.js
@@ -0,0 +1,13 @@
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const correoElectronico = document.getElementById('email');
+const celular = document.getElementById('celular ');
+const usuario = document.getElementById('usuario');
+const clave = document.getElementById('password');
+const fechaNacimiento = document.getElementById('fechaNacimiento');
+
+const const registrarse = () =>{
+    if (Input.value){
+        
+    }
+}
